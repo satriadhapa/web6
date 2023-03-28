47d5ad27f4d7e9011e107f20b23cfa50d08b470b
apollonsatria <satriadhapa10@gmail.com>
2023-03-28 11:33:15 +0700

    v2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        return view('dashboard');
+    }
+    public function gallery()
+    {
+        return view('gallery');
     }
 }
